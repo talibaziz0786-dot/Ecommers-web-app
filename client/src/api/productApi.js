1342ef9a46c0425d8801a62e0fed1c3df4445f36
Talib Aziz <talibaziz0786@gmail.com>
2026-05-10 14:53:10 +0530

    frontend deployment setup

--- client/src/api/productApi.js
+++ client/src/api/productApi.js
@@ -1,8 +1,28 @@
 import axios from "axios";
 
-const API = "http://localhost:5000/api/products";
+const API = axios.create({
+  baseURL: import.meta.env.VITE_API_URL,
+});
 
+
+// GET PRODUCTS
 export const fetchProducts = async () => {
-  const { data } = await axios.get(API);
+
+  const { data } =
+    await API.get("/products");
+
   return data;
 };
+
+
+// GET SINGLE PRODUCT
+export const fetchSingleProduct =
+  async (id) => {
+
+    const { data } =
+      await API.get(`/products/${id}`);
+
+    return data;
+};
+
+export default API;
